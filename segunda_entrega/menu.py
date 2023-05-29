43cd6ddc3b264bbe6575f78e1f3e6814b56c461b
Marcos Benavide <88857210+MarcosB0512@users.noreply.github.com>
2023-05-29 14:20:16 -0300

    actualizado 29052023

--- segunda_entrega/menu.py
+++ segunda_entrega/menu.py
@@ -2,7 +2,7 @@ from primer_paquete.alta_cliente import AdminCliente
 from primer_paquete.Segunda_pre_entrega import Cliente
 
 # Creo el cliente
-AdminCliente.__nuevo_cliente__("Silvia", "Kesta", "silvi@hotmail.com", "1234-5678")
+AdminCliente.__nuevo_cliente__("Marcos", "Benavide", "mbena@mail.com", "1234-5678")
 print()
 cliente = AdminCliente.listado_cliente()[0]
 
@@ -10,7 +10,7 @@ cliente = AdminCliente.listado_cliente()[0]
 print(f"\nel cliente creado es:\n{cliente}\n")
 
 # Modificar Cliente
-cliente_moificado = Cliente.actualizar_datos(cliente, "marcos", "Kesta", "marcosbenavide@hotmail.com", "999-9999")
+cliente_moificado = Cliente.actualizar_datos(cliente, "Marcos", "Benavide", "marcosbenavide72@gmail.com", "999-9999")
 
 # Agrego al carrito del cliente 
 compra = Cliente.agregar_carrito(cliente,"Zapatillas", 2)
